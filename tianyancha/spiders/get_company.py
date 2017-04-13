7adeb2a6ee61b560032ad0e7e346214ec6615722
mengguiyouziyi <775618369@qq.com>
2017-04-13 10:52:05 +0800

    init version

---
+++ tianyancha/spiders/get_company.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider
+import codecs
+import os
+
+
+class GetCompanySpider(CrawlSpider):
+    name = 'get_company'
+    allowed_domains = ['www.tianyancha.com']
+
+    def __init__(self):
+        company_path = os.path.join(os.getcwd(), 'company_list.txt')
+        with codecs.open(company_path, 'r', 'utf-8') as f:
+            self.company_list = f.readlines()
+
+    def start_requests(self):
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Connection': 'keep-alive',
+            'Content-Length': '0',
+            'Host': 'www.tianyancha.com',
+            'Upgrade-Insecure-Requests': '1',
+            'Referer': 'http://www.tianyancha.com/search',
+        }
+        urls = ['http://www.tianyancha.com/search?key=%s&checkFrom=searchBox' % str(self.company).strip() for self.company in self.company_list]
+        for url in urls:
+            print('request----url----：' + url)
+            yield scrapy.Request(url, headers=headers, callback=self.parse_item)
+
+    def parse_item(self, response):
+        print('response----url----：' + response.url)
+        # self.logger.info(response.body_as_unicode())
+        company_url_list = response.xpath('//a[contains(@class, "query_name") and contains(@class, "search-new-color")]/@href').extract()
+        # print('获取得到的公司url列表：' + str(company_url_list))
+        if len(company_url_list) > 0:
+            desired_url = company_url_list[0]
+            stock = response.xpath("//div[@class='add']/span/text()").extract()[0]
+            if '股票代码' == stock:
+                print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~%s~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~' % desired_url)
+                with codecs.open('company_url_list.txt', 'ab', 'utf-8') as file:
+                    file.write(desired_url + '\n')
+            else:
+                print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~首行非查询公司~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+                with codecs.open('fail.txt', 'ab', 'utf-8') as file:
+                    file.write(self.company + '\n')
+        else:
+            print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~获取查询列表为空~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+            with codecs.open('empty.txt', 'ab', 'utf-8') as file:
+                file.write(self.company + '\n')
+
+
+
+
+        # with codecs.open('%s.html' % str(self.company), 'wb', 'utf-8') as file:
+        #     file.writelines(response.body_as_unicode())
---
+++ tianyancha/spiders/get_detail.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.spiders import CrawlSpider
+import codecs
+
+
+class GetDetailSpider(CrawlSpider):
+    name = 'get_detail'
+    allowed_domains = ['www.tianyancha.com']
+    def start_requests(self):
+        headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, sdch',
+            'Accept-Language': 'zh-CN,zh;q=0.8',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Content-Length': '0',
+            'Host': 'www.tianyancha.com',
+            #'Origin': 'http://www.tianyancha.com',
+            'Upgrade-Insecure-Requests': '1',
+            # 'Referer': 'http://www.tianyancha.com/search?key=阿鲁科尔沁旗西古井子兴盛农牧专业合作社&checkFrom=searchBox',
+            'Referer': 'https://www.google.com/',
+        }
+        url = 'http://www.tianyancha.com/company/462746341'
+        yield scrapy.FormRequest(url, headers=headers, callback=self.parse_item)
+
+    def parse_item(self, response):
+        self.logger.info(response.url)
+        self.logger.info(response.body_as_unicode())
+        with codecs.open('462746341.html', 'wb', 'utf-8') as file:
+            file.writelines(response.body_as_unicode())
